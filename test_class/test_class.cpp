63dfd988134c4c68a1da464d05b217af1f9b0fe6
wangjun4 <wangjun4@iauto.com>
2020-09-03 15:01:30 +0800

    add test_class function

--- test_class/test_class.cpp
+++ test_class/test_class.cpp
@@ -5,6 +5,17 @@ using namespace std;
 class A {
     public:
         int num1;
+        int doublenumber() {
+            return num1 + pnum1;
+        }
+        A(){
+            num1 = 0;
+        }
+        A(int num){
+            num1 = num;
+        }
+    private:
+        int pnum1 = 2;
 };
 
 class B : public A {
@@ -25,9 +36,10 @@ int main()
 {
 
     cout << "test class A info start:" << endl;
+    //A classA(3);
     A classA;
-    classA.num1 = 3;
     cout << "class A number num1 = " << classA.num1 <<endl;
+    cout << "class A number double num1 = " << classA.doublenumber() <<endl;
     cout << "test class A info end\n" << endl;
 
     cout << "test class B info start:" << endl;
@@ -35,12 +47,14 @@ int main()
     classB.num1 = 2;
     classB.num2 = 3;
     cout << "class B number num1 = " << classB.num1 <<endl;
+    cout << "class B number double num1 = " << classB.doublenumber() <<endl;
     cout << "class B number num2 = " << classB.num2 <<endl;
     cout << "test class B info end \n" << endl;
 
     cout << "test class D info start:" << endl;
     D classD;
     cout << "class D number num1 = " << classD.num1 << endl;
+    cout << "class D number double num1 = " << classD.doublenumber() <<endl;
     cout << "class D number num3 = " << classD.num3 << endl;
     cout << "class D number num4 = " << classD.num4 << endl;
     cout << "test class D info end \n" << endl;
